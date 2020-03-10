9774760eeaa1cbeb2507775d9f01deb8c7a0246c
mrokiriko <mrokiriko@yandex.ru>
2020-03-10 22:58:32 +0700

    - use local storage for remembering seen posts - cleaner code

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,3 +1,6 @@
+let fetched_posts = {};
+let hidden_posts = {};
+
 chrome.extension.sendMessage({}, function(response) {
 
 	// ToDo use event or listener
@@ -6,6 +9,15 @@ chrome.extension.sendMessage({}, function(response) {
 			clearInterval(readyStateCheckInterval);
 
 			console.log("Расчехляю свой полифон...");
+
+			hidden_posts = JSON.parse(localStorage.getItem('hidden'));
+
+			if (hidden_posts == null){
+				hidden_posts = {};
+			}
+
+			console.log('hidden_posts');
+			console.log(hidden_posts);
 
 			if (window.location.href.indexOf('vk.com') !== -1){
 				console.log('its VK');
@@ -18,17 +30,7 @@ chrome.extension.sendMessage({}, function(response) {
 
 });
 
-let fetched_posts = {};
-let hidden_posts = {};
-
-function hide_news(post_id){
-	console.log(post_id);
-	alert(post_id);
-}
-
-async function hide_similar_posts(){
-	let post_elements = document.getElementsByClassName('feed_row');
-	
+function get_and_fetch_page_posts(post_elements){
 	let posts = [];
 
 	for (let i = 0; i < post_elements.length; i++){
@@ -40,9 +42,8 @@ async function hide_similar_posts(){
 			let post_text = post.getElementsByClassName('wall_post_text')[0];
 
 			// ToDo remove spaces
-			if (post_text !== undefined){
+			if (post_text !== undefined)
 				post_text = post_text.innerText.trim();
-			}
 
 			if (fetched_posts[post_id] === undefined && post_text !== undefined && post_text !== ''){
 
@@ -50,50 +51,80 @@ async function hide_similar_posts(){
 					text: post_text
 				};
 
-				posts.push(post_text);
+				// posts.push(post_text);
+				posts.push(post);
 			}
 		}
 	}
 
-	if (posts.length === 0){
-		return false;
-	}
+	return posts;
+}
 
-	let response = await checkPosts(posts);
-
+function hide_posts(response, post_elements){
 	for (let i = 0; i < response.length; i++){
 
-		if (response[i].success !== 'True'){
+		if (response[i].success !== true)
 			continue;
-		}
 
 		let post_id = response[i].msg;
 
 		if (post_id > 0){
-
 			if (hidden_posts[post_id]){
 
 				let el = post_elements[i];
-
 				console.log('Новость скрыта. ' + post_id);
 				console.log(el);
 
-				// create hidden button 
-				let post_author = el.children[0].getElementsByClassName('author')[0].innerText;
-				let command = "document.getElementById('" + post_id + "').getElementsByClassName('post_content')[0].style.display = 'block';";
-				command += "document.getElementById('" + post_id + "').getElementsByClassName('author')[0].innerText = '" + post_author + "';";
-
-				el.children[0].getElementsByClassName('_post_content')[0].setAttribute('onclick', command);
-				el.children[0].getElementsByClassName('post_header')[0].style.padding = '15px 20px 15px';
-				el.children[0].getElementsByClassName('author')[0].innerText = 'Show similar news';
-				el.children[0].getElementsByClassName('post_content')[0].style.display = 'none';
-
+				create_hide_post_button(el);
 			} else {
 				hidden_posts[post_id] = true;
 			}
 		}
-		
 	}
+
+	localStorage.setItem('hidden', JSON.stringify(hidden_posts));
+}
+
+function create_hide_post_button(el){
+	// create hidden button
+	let post_author = el.getElementsByClassName('author')[0].innerText;
+	let id = el.getAttribute('id');
+
+	let command = "document.getElementById('" + id + "').getElementsByClassName('post_content')[0].style.display = 'block';";
+	command += "document.getElementById('" + id + "').getElementsByClassName('author')[0].innerText = '" + post_author + "';";
+
+	el.getElementsByClassName('_post_content')[0].setAttribute('onclick', command);
+	el.getElementsByClassName('post_header')[0].style.padding = '15px 20px 15px';
+	el.getElementsByClassName('author')[0].innerText = 'Show similar news';
+	el.getElementsByClassName('post_content')[0].style.display = 'none';
+}
+
+async function hide_similar_posts(){
+	let post_elements = document.getElementsByClassName('feed_row');
+	let posts = get_and_fetch_page_posts(post_elements);
+
+	let post_texts = [];
+
+	for (let i = 0; i < posts.length; i++){
+		let post = posts[i];
+		let post_id = post.getAttribute('id');
+
+		if (post_id !== null && post_id.includes('ads') !== true){
+			let post_text = post.getElementsByClassName('wall_post_text')[0];
+
+			// ToDo remove spaces
+			if (post_text !== undefined)
+				post_text = post_text.innerText.trim();
+
+			post_texts.push(post_text);
+		}
+	}
+
+	if (post_texts.length === 0)
+		return false;
+
+	let response = await checkPosts(post_texts);
+	hide_posts(response, posts);
 }
 
 
@@ -102,7 +133,7 @@ async function checkPosts(posts) {
 	let link = 'https://gtusur.pythonanywhere.com/api/articles/';
 
 	try {
-		texts = [];
+		let texts = [];
 
 		for (let i = 0; i < posts.length; i++){
 			texts.push({
